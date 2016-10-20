b57f9d69b6d204e89554f45cba57fd9fd7303a7b
a.g.mcclelland <a.g.mcclelland@accenture.com>
2016-10-20 13:28:43 +0100

    Add Vector2D class

--- src/com/utility/Vector2D.java
+++ src/com/utility/Vector2D.java
@@ -1,42 +1,97 @@
 package com.utility;
 
 public class Vector2D {
-	public float x,y;
-	
-	public float getDistance(Vector2D otherVector)
-	{
-		float distX = otherVector.x - x;
-		float distY = otherVector.y - y;
-		
-		return (float) Math.sqrt((distX * distX) + (distY * distY));
+
+	public float x;
+	public float y;
+
+	public Vector2D(float _x, float _y) {
+		x = _x;
+		y = _y;
 	}
-	
-	public float getMagnitude()
-	{
-		return (float) Math.sqrt((x * x) + (y * y));
+
+	public Vector2D() {
 	}
-	
-	//TODO: Implement these functions
-	
-	public float getSqrMagnitude()
-	{
-		return (x*x) + (y*y);
+
+	public Vector2D(Vector2D vector) {
+		x = vector.x;
+		y = vector.y;
 	}
-	
-	/*
-	//Note - this will normalize the vector
-	public Vector2D normalize();
-	
-	public Vector2D mult();
-	
-	public Vector2D add();
-	
-	public Vector2D sub();
-	
-	public Vector2D div();
-	
-	public Vector2D limit();
-	
-	public String toString();
-	*/
+
+	public float dist(Vector2D v) {
+		Vector2D diff = Vector2D.sub(v, this);
+		return diff.mag();
+	}
+
+	public float mag() {
+		return (float) Math.sqrt(x * x + y * y);
+	}
+
+	public float sqrMag() {
+		return x * x + y * y;
+	}
+
+	public void add(Vector2D v) {
+		x += v.x;
+		y += v.y;
+	}
+
+	public void sub(Vector2D v) {
+		x -= v.x;
+		y -= v.y;
+	}
+
+	public static Vector2D add(Vector2D a, Vector2D b) {
+		Vector2D c = new Vector2D(a.x + b.x, a.y + b.y);
+		return c;
+	}
+
+	public static Vector2D sub(Vector2D a, Vector2D b) {
+		Vector2D c = new Vector2D(a.x - b.x, a.y - b.y);
+		return c;
+	}
+
+	public void mult(Vector2D v) {
+		x *= v.x;
+		y *= v.y;
+	}
+
+	public void mult(float m) {
+		x *= m;
+		y *= m;
+	}
+
+	public void div(float d) {
+		x /= d;
+		y /= d;
+	}
+
+	public void div(Vector2D v) {
+		x /= v.x;
+		y /= v.y;
+	}
+
+	public void normalize() {
+		float m = mag();
+		if (m != 0) {
+			div(m);
+		}
+	}
+
+	public void limit(float max) {
+		if (mag() > max) {
+			setMag(max);
+		}
+	}
+
+	public void setMag(float m) {
+		normalize();
+		mult(m);
+	}
+
+	@Override
+	public String toString() {
+		return "(" + x + ", " + y + ")";
+	}
+
 }
